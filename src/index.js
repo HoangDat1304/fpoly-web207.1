934f8e1e0f96333ab2d9f94ab8060ac3b82e2844
HoangDat1304 <HoangDat130401@gmail.com>
2021-07-01 07:59:47 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const person={
+  name : 'Hoàng Văn Đạt',
+  age :  20,
+  status : false
+}
+function total(giatriA,giatriB){
+  return giatriA + giatriB;
+}
+const a = 5;
+const b = 5;
+
+const element = <h1>Tổng là :{total(a,b)}</h1>
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
+
+
