5366809c2d0c581b4814f81d48784813c42a87f2
HoangDat1304 <HoangDat130401@gmail.com>
2021-07-01 09:07:40 +0700

    add App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App(){
+    function onclickHelloHandler(){
+        alert("Chao con vo!");
+    }
+    return(
+        <buttom onClick={onclickHelloHandler}>Hello</buttom>
+    );
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-
-const person={
-  name : 'Hoàng Văn Đạt',
-  age :  20,
-  status : false
+const person = {
+  name: 'Hoàng Văn Đạt',
+  age: 20,
+  status: false
 }
-function total(giatriA,giatriB){
+function total(giatriA, giatriB) {
   return giatriA + giatriB;
 }
 const a = 5;
 const b = 5;
 
-const element = <h1>Tổng là :{total(a,b)}</h1>
+const element = <h1>Tổng là :{total(a, b)}</h1>
 
 ReactDOM.render(
-  element,
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
